c3287bbe913551e38f91f0a6087595170654dfa2
andyLue <laserlue@gmail.com>
2021-04-22 12:22:14 +0800

    push test2

--- server/routes/test_route.js
+++ server/routes/test_route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.route('/test').get((req, res) => {
     console.log('test');
-    // res.send('test');
+    res.send('test');
 });
 
 module.exports = router;
