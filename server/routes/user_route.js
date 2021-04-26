90e2fa1480ac83ea77de583d9bd1b6215ba1614b
andyLue <laserlue@gmail.com>
2021-04-26 17:16:06 +0800

    modify orderhistory route

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -11,6 +11,14 @@ const {
     getUserProfile,
 } = require('../controllers/user_controller');
 
+const {
+    getOrderHistory,
+} = require('../controllers/order_controller');
+
+const {
+    USER_ROLE
+} = require('../models/user_model');
+
 router.route('/user/signup')
     .post(wrapAsync(signUp));
 
@@ -19,5 +27,7 @@ router.route('/user/signin')
 
 router.route('/user/profile')
     .get(authentication(), wrapAsync(getUserProfile));
+
+router.route('/user/orderhistory').get(authentication(USER_ROLE.ALL), wrapAsync(getOrderHistory));
 
 module.exports = router;
